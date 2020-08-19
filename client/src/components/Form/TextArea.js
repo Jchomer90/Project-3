347ad6265a01350e03fefcadec9621eee52c4afd
Chris <christopheraturton@gmail.com>
2020-08-19 12:54:27 -0400

    file fix

--- client/src/components/Form/TextArea.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function TextArea(props) {
-  return (
-    <div className="form-group">
-      <textarea className="form-control" rows="20" {...props} />
-    </div>
-  );
-}
